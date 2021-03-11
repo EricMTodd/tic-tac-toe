8cc25516fc3492a57103eb6c51f9673e944cd74c
EricMTodd <eterictodd@gmail.com>
2021-03-11 00:29:23 -0700

    Framework for initial welcome page created.

--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,43 @@
-const app = (() => {
-  console.log("app.js successfully loaded.");
-  let peopleList = [];
+const app = ((doc) => {
+  if (document) {
+    console.log("app.js successfully loaded.");
+    let peopleList = [];
+  
+    let postCount = 0;
+    const print = (str) => {
+      let mainDiv = document.querySelector("#main");
+      let newPrintStatement = document.createElement("div");
+      newPrintStatement.innerHTML = `<h1 id=${postCount}>${str}</h1>`;
+      postCount++;
+      mainDiv.appendChild(newPrintStatement);
+    }
+  
+    let personCount = 0;
+    const person = (name, age, gender) => {
+      let newPerson = {
+        id: personCount,
+        name,
+        age,
+        gender,
+      };
+      personCount++;
+      peopleList.push(newPerson);
+    }
 
-  let postCount = 0;
-  const print = (str) => {
-    let mainDiv = document.querySelector("#main");
-    let newPrintStatement = document.createElement("div");
-    newPrintStatement.innerHTML = `<h1 id=${postCount}>${str}</h1>`;
-    postCount++;
-    mainDiv.appendChild(newPrintStatement);
+    // Create local storage objects to tie each website
+    let library = {
+      initialVisit: true,
+      uniqueId: 3,
+      catalogue: {}
+    }
+  
+    return {
+      print,
+      person,
+      peopleList,
+    }
+  } else {
+    console.log("Loading document...");
   }
 
-  let personCount = 0;
-  const person = (name, age, gender) => {
-    let newPerson = {
-      id: personCount,
-      name,
-      age,
-      gender,
-    };
-    personCount++;
-    peopleList.push(newPerson);
-  }
-
-  return {
-    print,
-    person,
-    peopleList,
-  }
-})();
+})(document);
--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,40 +1,65 @@
-const ticTacToe = (() => {
-  console.log("tic-tac-toe.js successfully loaded.");
-  let playerList = [];
-
-  const player = (name) => {
-    let newPlayer = {
-      name,
-      wins: 0,
-      losses: 0,
-      created: Date(),
+const ticTacToe = ((doc) => {
+  if (doc) {
+    console.log("tic-tac-toe.js successfully loaded.");
+    let playerList = [];
+  
+    const player = (name) => {
+      let newPlayer = {
+        name,
+        wins: 0,
+        losses: 0,
+        created: Date(),
+      }
+      playerList.push(newPlayer);
     }
-    playerList.push(newPlayer);
+  
+    const welcome = (() => {
+      // Display welcome page/leaderboard
+      let mainDiv = document.querySelector("#main");
+      // Check local storage for users.
+      if (!localStorage.ticTacToe) {
+        console.log("Displaying initial welcome page.");
+        mainDiv.innerHTML = `
+        <div id="initial-landing-page">
+          <form>
+            <fieldset>
+              <legend>
+                Create New User
+              </legend>
+              <br>
+              <input placeholder="Name">
+              <br><br>
+              <button>Submit</button>
+            </fieldset>
+          </form>
+        </div>
+        `;
+      } else {
+        console.log("Hmmmm...");
+      }
+      // If users are present, display leaderboard.
+      // Else, ask to create a user!
+    })();
+  
+    const gameLoop = () => {
+      // Game logic for placing symbols goes here!
+    }
+  
+    const endTurn = () => {
+      // Logic for switching symbols and player turns.
+    }
+  
+    const evaluateRound = () => {
+      // Check for win condition
+    }
+  
+    return {
+      playerList,
+      player,
+      welcome,
+    }
+  } else {
+    console.log("Loading document...");
   }
 
-  const welcome = (() => {
-    // Display welcome page/leaderboard
-    let mainDiv = document.querySelector("#main");
-    // Check local storage for users.
-    // If users are present, display leaderboard.
-    // Else, ask to create a user!
-  })();
-
-  const gameLoop = () => {
-    // Game logic for placing symbols goes here!
-  }
-
-  const endTurn = () => {
-    // Logic for switching symbols and player turns.
-  }
-
-  const evaluateRound = () => {
-    // Check for win condition
-  }
-
-  return {
-    playerList,
-    player,
-    welcome,
-  }
-})();
+})(document);
