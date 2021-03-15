a97c65bb4ddb333a73cb9d0065c792fffdea67fb
Eric Todd <eterictodd@gmail.com>
2021-03-15 16:48:16 -0600

    Working on seperation of concerns.

--- controllers/users.js
+++ controllers/users.js
@@ -1,30 +1,37 @@
 const usersController = (() => {
 
   const createUser = (name) => {
-    let obj = JSON.parse(localStorage.ticTacToe);
-    let leaderboardUsersList = document.querySelector("#leaderboard-users-list");
+    // let obj = JSON.parse(localStorage.ticTacToe);
+    // let leaderboardUsersList = document.querySelector("#leaderboard-users-list");
 
+    // Create new user with prototypal inheritance and con
+    let newUser = ({ user }) => {
+      console.log(user)
+    }
 
-    if (name != "") {
-      ((newUser, obj) => {
-        obj.usersList.push(newUser);
-        obj.uniqueId++;
-        let str = JSON.stringify(obj);
-        localStorage.setItem("ticTacToe", str);
-        return document.querySelector("#create-new-user-name-input").value = "";
-      })(newUser, obj);
+    // if (name != "") {
+    //   ((newUser, obj) => {
+    //     obj.usersList.push(newUser);
+    //     obj.uniqueId++;
+    //     let str = JSON.stringify(obj);
+    //     localStorage.setItem("ticTacToe", str);
+    //     return document.querySelector("#create-new-user-name-input").value = "";
+    //   })(newUser, obj);
 
-      ((newUser, leaderboardUsersList) => {
-        let userListItem = document.createElement("li");
-        userListItem.id = newUser.id;
-        userListItem.innerText = newUser.name;
-        return leaderboardUsersList.appendChild(userListItem);
-      })(newUser, leaderboardUsersList);
-    } else {
-      alert("Please enter a valid username.")
+    //   ((newUser, leaderboardUsersList) => {
+    //     let userListItem = document.createElement("li");
+    //     userListItem.id = newUser.id;
+    //     userListItem.innerText = newUser.name;
+    //     return leaderboardUsersList.appendChild(userListItem);
+    //   })(newUser, leaderboardUsersList);
+    // } else {
+    //   alert("Please enter a valid username.")
+    // }
+    return {
+      newUser,
     }
   }
   return {
-    // createUser,
+    createUser,
   }
 })();
---
+++ scripts/landingPage.js
@@ -0,0 +1,14 @@
+const renderLeaderboard = (() => {
+  let leaderboardUsersList = document.querySelector("#leaderboard-users-list");
+  let obj = JSON.parse(localStorage.ticTacToe);
+  for (let i = 0; i < obj.usersList.length; i++) {
+    let userListItem = document.createElement("li");
+    userListItem.id = obj.usersList[i].id;
+    userListItem.innerText = obj.usersList[i].name;
+    leaderboardUsersList.appendChild(userListItem);
+  }
+  return;
+})();
+
+
+
