49d173f9fd57192af319f7ecafd69b5121cfa29e
EricMTodd <eterictodd@gmail.com>
2021-03-16 11:58:48 -0600

    Salt generated.

--- controllers/encryption.js
+++ controllers/encryption.js
@@ -1,10 +1,23 @@
 const encryptionController = (() => {
 
-  const encrypt = () => {};
+  const generateSalt = () => {
+    const alphanumerics = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let saltLength = Math.floor((Math.random() * 21) + 1);
+    let saltKey = "";
+    for (let i = 0; i < saltLength; i++) {
+      let randomNumber = Math.floor(Math.random() * 61);
+      saltKey += alphanumerics[randomNumber];
+    };
+    return saltKey;
+  };
+
+  const encrypt = (password, salt) => {
+    // Use salt
+  };
 
   const decrypt = () => {};
 
   return {
-
+    generateSalt,
   };
 })();
