fb8cbce2ba000d15623a02a257cfb6b351de8091
EricMTodd <eterictodd@gmail.com>
2021-03-16 12:39:32 -0600

    Encryption nearly complete.

--- controllers/encryption.js
+++ controllers/encryption.js
@@ -1,21 +1,37 @@
 const encryptionController = (() => {
-
+  const alphanumerics = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let password = "password";
+  
   const generateSalt = () => {
-    const alphanumerics = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let saltLength = Math.floor((Math.random() * 21) + 1);
-    let saltKey = "";
+    let saltKey = [];
+    let salt = "";
     for (let i = 0; i < saltLength; i++) {
       let randomNumber = Math.floor(Math.random() * 61);
-      saltKey += alphanumerics[randomNumber];
+      saltKey.push(randomNumber);
+      salt += alphanumerics[randomNumber];
     };
-    return saltKey;
+    return encrypt(password, salt, saltKey);
   };
 
-  const encrypt = (password, salt) => {
-    // Use salt
+  const encrypt = (password, salt, saltKey) => {
+    console.log(`password: ${password}\nsalt: ${salt}\nsaltKey:`, saltKey);
+    let encryptedPassword = "";
+    let passwordKey = [];
+    for (let i = 0; i < password.length; i++) {
+      // console.log(password[i]);
+      for (let k = 0; k < alphanumerics.length; k++) {
+        // console.log(alphanumerics[k]);
+        if (password[i] == alphanumerics[k]) {
+          console.log("Match");
+        }
+      }
+    };
   };
 
   const decrypt = () => {};
+
+  generateSalt();
 
   return {
     generateSalt,
