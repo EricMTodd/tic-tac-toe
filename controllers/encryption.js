d6995141be207408c9b97ac1ab0dad5c9c137bb8
Eric Todd <eterictodd@gmail.com>
2021-03-18 22:54:55 -0600

    Moving to desktop.

--- controllers/encryption.js
+++ controllers/encryption.js
@@ -1,7 +1,28 @@
 const encryptionController = (() => {
+  const alphanumerics = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
+  const password = "GetRektLoser69420";
+  const shift = "pilot";
 
   const encrypt = () => {
-    // Just use a static phrase and key for now.
+    let phraseLettersIndexes = [];
+    let shiftLettersIndexes = [];
+
+    for (let i = 0; i < password.length; i++) {
+      for (let j = 0; j < alphanumerics.length; j++) {
+        if (password[i] == alphanumerics[j]) {
+          phraseLettersIndexes.push(j);
+          console.log(`${password[i]}: ${j}`)
+        }
+      }
+    }
+    console.log(phraseLettersIndexes);
+
+    // for (let i = 0; i < shift.length; i++) {
+    //   if (i > shift.length)
+    // }
+
+
+
   };
 
   const decrypt = () => {
@@ -9,6 +30,6 @@ const encryptionController = (() => {
   };
 
   return {
-
+    encrypt,
   };
 })();
