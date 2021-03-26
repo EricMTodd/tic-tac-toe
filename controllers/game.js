a2b809c7d6e42b8005ee49a50caccd9921dcfd55
Eric M. Todd <eterictodd@gmail.com>
2021-03-25 18:55:48 -0600

    Login bugs ironed out.

--- controllers/game.js
+++ controllers/game.js
@@ -1,14 +1,18 @@
 const gameController = (() => {
-  console.log("game.js successfully loaded.");
   let storageObject = JSON.parse(localStorage.ticTacToe);
 
-  const verifyUsers = () => {
+  const verifyUsers = (() => {
     if (storageObject.activePlayerOne === null || storageObject.activePlayerTwo === null) {
-      console.log("Players are required to start");
-    }
+      console.log("suck it");
+      let statusMessage = document.createElement("p");
+      statusMessage.innerText = "Two players must be logged in to play.";
+      document.querySelector("#game-mode-selection-container").appendChild(statusMessage);
+    } else {
+      document.querySelector("#start-game-button").disabled = false;
+    };
     return;
     // End of verifyUsers function
-  };
+  })();
 
   return {
     verifyUsers,
