245d9e0dfd8cbb8103e63b0f73a64158622188ee
Eric M. Todd <eterictodd@gmail.com>
2021-03-31 23:13:55 -0600

    Working on sorting users by wins.

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,18 +1,66 @@
 const viewsController = (() => {
   let storageObject = JSON.parse(localStorage.ticTacToe);
+  usersList = [];
+
+  const sortUsersByWins = (() => {
+    let sortedList = [];
+    usersList = [
+      {
+        name: "Caitlyn",
+        wins: 4,
+      },
+      {
+        name: "Dorian",
+        wins: 3,
+      },
+      {
+        name: "Eric",
+        wins: 5,
+      },
+      {
+        name: "Silver",
+        wins: 3,
+      },
+      {
+        name: "Markham",
+        wins: 1,
+      },
+      {
+        name: "Will",
+        wins: 2,
+      },
+    ];
+    let highScoringPlayer = usersList[0];
+    let spliceIndex = 0;
+    let listLength = 6;
+    // Find highest score
+
+    while (listLength > 0) {
+
+      for (let i = 0; i < usersList.length; i++) {
+        if (usersList[i].wins > highScoringPlayer.wins) {
+          highScoringPlayer = usersList[i];
+          spliceIndex = i;
+        }
+      }
+      sortedList.push(highScoringPlayer);
+      usersList.splice(spliceIndex, 1);
+      listLength--;
+      highScoringPlayer = usersList[0];
+    }
+
+    for (let i = 0; i < sortedList.length; i++) {
+      // console.log(`usersList[i].name: ${usersList[i].name}`);
+      console.log(`sortedList[i].name: ${sortedList[i].name}\nsortedList[i].wins: ${sortedList[i].wins}`)
+    }
+
+  })();
 
   
   const renderLeaderboard = (() => {
-    let leaderboardUsersList = document.querySelector("#leaderboard-users-list");
-
-    // Loop over users and display them
-    for (let i = 0; i < storageObject.usersList.length; i++) {
-      let userListItem = document.createElement("li");
-      userListItem.id = storageObject.usersList[i].id;
-      userListItem.innerText = storageObject.usersList[i].name;
-      leaderboardUsersList.appendChild(userListItem);
-    }
+    // Render a list of users sorted by wins
     return;
+    // End of renderLeaderboard function
   })();
 
   const verifyUsers = (() => {
