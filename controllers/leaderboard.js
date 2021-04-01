614db534727f08e240afe873c59021ccd421c99c
EricMTodd <eterictodd@gmail.com>
2021-04-01 12:09:02 -0600

    MVP achieved.

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,82 +1,59 @@
 const viewsController = (() => {
-  let storageObject = JSON.parse(localStorage.ticTacToe);
-  usersList = [];
+  let storageObject = JSON.parse(localStorage.ticTacToe)
+
+  const renderLeaderboard = (arr) => {
+    // Render a list of users sorted by wins
+    let leaderboardUsersList = document.querySelector("#leaderboard-users-list")
+    for (let i = 0; i < arr.length; i ++) {
+      let listItem = document.createElement("li")
+      listItem.innerText = `${i + 1}. Player Name: ${arr[i].name}\nLifetime Wins: ${arr[i].wins}`
+      leaderboardUsersList.appendChild(listItem)
+    }
+    return
+    // End of renderLeaderboard function
+  }
 
   const sortUsersByWins = (() => {
-    let sortedList = [];
-    usersList = [
-      {
-        name: "Caitlyn",
-        wins: 4,
-      },
-      {
-        name: "Dorian",
-        wins: 3,
-      },
-      {
-        name: "Eric",
-        wins: 5,
-      },
-      {
-        name: "Silver",
-        wins: 3,
-      },
-      {
-        name: "Markham",
-        wins: 1,
-      },
-      {
-        name: "Will",
-        wins: 2,
-      },
-    ];
-    let highScoringPlayer = usersList[0];
-    let spliceIndex = 0;
-    let listLength = 6;
-    // Find highest score
+    let usersList = storageObject.usersList
+    let listLength = storageObject.usersList.length
+    let sortedList = []
+    let highScore = 0
+    let user = usersList[0]
+    let spliceIndex = 0
 
-    while (listLength > 0) {
-
+    for (let i = 0; i < listLength; i++) {
       for (let i = 0; i < usersList.length; i++) {
-        if (usersList[i].wins > highScoringPlayer.wins) {
-          highScoringPlayer = usersList[i];
-          spliceIndex = i;
+        if (usersList[i].wins > highScore) {
+          highScore = usersList[i].wins
+          user = usersList[i]
+          spliceIndex = i
         }
       }
-      sortedList.push(highScoringPlayer);
-      usersList.splice(spliceIndex, 1);
-      listLength--;
-      highScoringPlayer = usersList[0];
+      sortedList.push(user)
+      usersList.splice(spliceIndex, 1)
+      user = usersList[0]
+      highScore = 0
+      spliceIndex = 0  
     }
 
-    for (let i = 0; i < sortedList.length; i++) {
-      // console.log(`usersList[i].name: ${usersList[i].name}`);
-      console.log(`sortedList[i].name: ${sortedList[i].name}\nsortedList[i].wins: ${sortedList[i].wins}`)
-    }
-
-  })();
-
-  
-  const renderLeaderboard = (() => {
-    // Render a list of users sorted by wins
-    return;
-    // End of renderLeaderboard function
-  })();
+    return renderLeaderboard(sortedList)
+    // End of findUserByWins function
+  })()
 
   const verifyUsers = (() => {
     if (storageObject.activePlayerOne === null || storageObject.activePlayerTwo === null) {
-      let statusMessage = document.createElement("p");
-      statusMessage.innerText = "Two players must be logged in to play.";
-      document.querySelector("#game-mode-selection-container").appendChild(statusMessage);
+      let statusMessage = document.createElement("p")
+      statusMessage.innerText = "Two players must be logged in to play."
+      document.querySelector("#game-mode-selection-container").appendChild(statusMessage)
     } else {
-      document.querySelector("#start-game-button").disabled = false;
-    };
-    return;
+      document.querySelector("#start-game-button").disabled = false
+    }
+    return
     // End of verifyUsers function
-  })();
+  })()
 
   return {
     renderLeaderboard,
     verifyUsers,
-  };
-})();
+  }
+})()
